86d380ba5c183606f8e44403a922d998916a8038
ParadoxLike <robert-kiyamov@mail.ru>
2020-07-25 23:26:44 +0400

    Updates

---
+++ precache-manifest.31e41e5bc4bbea33da565aedd0c47447.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcdd18880fa591d79c6a2bf82547e8d3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "453408a67f2db8c02a5b",
+    "url": "./static/css/2.1e562f28.chunk.css"
+  },
+  {
+    "revision": "453408a67f2db8c02a5b",
+    "url": "./static/js/2.a1db3004.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a1db3004.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03aee5633e7cb9486d4d",
+    "url": "./static/js/main.6528ce14.chunk.js"
+  },
+  {
+    "revision": "705f434324a29d64b932",
+    "url": "./static/js/runtime-main.075efe5b.js"
+  }
+]);
